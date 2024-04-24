c59495dcf849560f2c15e76d33ba208653187bc8
Kim Ji Yong <jykim00324@gmail.com>
2024-04-24 17:44:36 +0900

    [FE] Chore: change description

--- frontend/src/app/(main)/room/[id]/page.tsx
+++ frontend/src/app/(main)/room/[id]/page.tsx
@@ -1,7 +1,7 @@
 export default function RoomPage({ params }: { params: { id: number } }) {
   return (
     <main>
-      <h1>{params.id}번 방 페이지 </h1>
+      <h1>{params.id}번 방 페이지입니다.</h1>
     </main>
   )
 }
