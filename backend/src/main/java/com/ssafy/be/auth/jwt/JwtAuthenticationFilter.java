469667e75fae090c9151dc71b723ab996eff9b36
박근수 <rmstn8426@gmail.com>
2024-04-25 11:06:45 +0900

    [be] feat: port 8080 -> 8081

---
+++ backend/src/main/java/com/ssafy/be/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,20 @@
+package com.ssafy.be.auth.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
