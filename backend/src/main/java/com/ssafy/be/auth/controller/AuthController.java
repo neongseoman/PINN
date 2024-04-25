dbfef8c15c5164c0b616a5501dba61016ef4845d
박근수 <rmstn8426@gmail.com>
2024-04-25 11:00:18 +0900

    [be] feat: add test api api/v1/oauth/test

--- backend/src/main/java/com/ssafy/be/auth/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/be/auth/controller/AuthController.java
@@ -11,12 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.ssafy.be.common.response.BaseResponseStatus.SUCCESS;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("api/v1/oauth")
 public class AuthController {
     @Autowired
     AuthService authService;
 
-    @GetMapping("")
+    @GetMapping("kakao")
+    public BaseResponse<?> getKakaoToken(){
+
+        return new BaseResponse<>(SUCCESS);
+    }
+
+    @GetMapping("test")
     public BaseResponse<?> test(){
         return new BaseResponse<>(SUCCESS);
     }
