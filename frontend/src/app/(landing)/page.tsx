f789130c2e6bd037166d12a74e28e29d5c664c38
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-04-26 14:33:07 +0900

    fe feat: add kakao login link

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import styles from './landing.module.css'
+import { FaAnglesDown } from "react-icons/fa6";
 
-export default function LandingPage() {
+export default function KakaoLogin() {
+
   return (
-    <main className={styles.green}>
-      <h1>랜딩페이지</h1>
+    <main className={styles.landing}>
+      <p className={styles.logo}>지도 게임</p>
+      <p className={styles.name}>PINN</p>
+      <Link href={'https://kauth.kakao.com/oauth/authorize?client_id=a4bfe3f2b2f815648b923deb0a3c54c7&redirect_uri=http://www.pinn.kr/api/v1/oauth/kakao&response_type=code'} className={styles.login}>카카오 로그인</Link>
+      <button className={styles.scroll}><FaAnglesDown /></button>
     </main>
   )
 }
