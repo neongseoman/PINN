6f98b1a43d5e458e6c306b2dea99dd05c0d5db3e
Kim Ji Yong <jykim00324@gmail.com>
2024-04-26 14:16:55 +0900

    [fe] feat: add variable base prop

--- frontend/src/app/(main)/game/page.tsx
+++ frontend/src/app/(main)/game/page.tsx
@@ -7,17 +7,19 @@ import styles from './game.module.css'
 
 export default function GamePage() {
   const hints = ['hint 1', 'hint 2', 'hint 3']
+  const theme = 'random'
+  const initialTime = 100
   return (
     <main>
       <div className={styles.infos}>
-        <GameInfo theme={'random'} round={1} stage={1} />
-        <ThemeInfo theme={'random'} />
+        <GameInfo theme={theme} round={1} stage={1} />
+        <ThemeInfo theme={theme} />
       </div>
       <div className={`${styles.hints} ${styles.opacity}`}>
-        <Hints hints={hints} theme={'random'} />
+        <Hints hints={hints} theme={theme} />
       </div>
       <div className={styles.timer}>
-        <Timer initialTime={97} theme={'random'} />
+        <Timer initialTime={initialTime} theme={theme} />
       </div>
       <div className={`${styles.chat} ${styles.opacity}`}>Chatting</div>
       <div className={`${styles.map} ${styles.opacity}`}>Map</div>
