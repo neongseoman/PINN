6181a9dd7fb8341ffd609dad680a1c84323ac003
Kim Ji Yong <jykim00324@gmail.com>
2024-04-26 13:59:46 +0900

    [fe] design: add skeleton ingame design

--- frontend/src/app/(main)/game/page.tsx
+++ frontend/src/app/(main)/game/page.tsx
@@ -1,7 +1,26 @@
+import Timer from '@/app/components/Timer'
+import ThemeInfo from './_components/ThemeInfo'
+import GameInfo from './_components/GameInfo'
+import Hints from './_components/Hints'
+
+import styles from './game.module.css'
+
 export default function GamePage() {
+  const hints = ['hint 1', 'hint 2', 'hint 3']
   return (
     <main>
-      <h1>게임 페이지</h1>
+      <div className={styles.infos}>
+        <GameInfo theme={'random'} round={1} stage={1} />
+        <ThemeInfo theme={'random'} />
+      </div>
+      <div className={`${styles.hints} ${styles.opacity}`}>
+        <Hints hints={hints} theme={'random'} />
+      </div>
+      <div className={styles.timer}>
+        <Timer initialTime={97} theme={'random'} />
+      </div>
+      <div className={`${styles.chat} ${styles.opacity}`}>Chatting</div>
+      <div className={`${styles.map} ${styles.opacity}`}>Map</div>
     </main>
   )
 }
