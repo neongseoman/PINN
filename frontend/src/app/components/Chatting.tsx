6f3b6df78c9a2d7a7d19417ae15460052015c63d
Kim Ji Yong <jykim00324@gmail.com>
2024-04-29 15:19:19 +0900

    [fe] feat: change socket.io code to stomp code

--- frontend/src/app/components/Chatting.tsx
+++ frontend/src/app/components/Chatting.tsx
@@ -1,28 +1,62 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
-import io from 'socket.io-client'
+import { useState, useEffect } from 'react'
+import { Client, IFrame, IMessage } from '@stomp/stompjs'
 import styles from './chatting.module.css'
+
+interface MessageFormat {
+  content: string
+}
 
 export default function Chatting() {
   const [messages, setMessages] = useState<string[]>([])
   const [newMessage, setNewMessage] = useState<string>('')
 
-  const socket = io('http://localhost:3000')
+  const socketUrl = 'aa'
+  const subscribeUrl = 'aa'
+  const destinationUrl = 'aa'
+
+  const client = new Client({
+    brokerURL: socketUrl,
+    connectHeaders: {
+      login: 'user',
+      passcode: 'password',
+    },
+    debug: function (str: string) {
+      console.log(str)
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+  })
 
   useEffect(() => {
-    socket.on('message', (message: string) => {
-      setMessages((prevMessages) => [...prevMessages, message])
-    })
+    client.onConnect = function (_frame: IFrame) {
+      client.subscribe(subscribeUrl, (message: IMessage) => {
+        const messageResponse = JSON.parse(message.body) as MessageFormat
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          messageResponse.content,
+        ])
+      })
+    }
 
+    client.onStompError = function (frame: IFrame) {
+      console.log('Broker reported error: ' + frame.headers['message'])
+      console.log('Additional details: ' + frame.body)
+    }
+
+    client.activate()
     return () => {
-      socket.disconnect()
+      client.deactivate() // Cleanup on unmount
     }
   }, [])
 
   const handleSendMessage = () => {
-    socket.emit('message', newMessage)
-    setNewMessage('')
+    client.publish({
+      destination: destinationUrl,
+      body: JSON.stringify({ newMessage: newMessage }),
+    })
   }
 
   return (
