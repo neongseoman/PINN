732e6320dbab8573d3d729d18e1538522bfe4df3
Kim Ji Yong <jykim00324@gmail.com>
2024-04-30 14:39:46 +0900

    [fe] feat: add streetview and map

--- frontend/src/app/(main)/game/_components/IngameMap.tsx
+++ frontend/src/app/(main)/game/_components/IngameMap.tsx
@@ -1,14 +1,41 @@
+'use client'
+
 import styles from './ingamemap.module.css'
 import themeStyles from '@/app/components/theme.module.css'
+// import { Loader } from '@googlemaps/js-api-loader'
+import { useEffect, useRef } from 'react'
 
 interface IngameMapProps {
   theme: string
+  loader: any
 }
 
-export default function IngameMap({ theme }: IngameMapProps) {
+export default function IngameMap({ theme, loader }: IngameMapProps) {
+  const mapRef = useRef<any>()
+  // const loader = new Loader({
+  //   apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string,
+  //   version: 'weekly',
+  //   // ...additionalOptions,
+  // })
+
+  useEffect(() => {
+    loader.importLibrary('maps').then(async () => {
+      const position = { lat: 37.5642135, lng: 127.0016985 }
+      const { Map } = (await google.maps.importLibrary(
+        'maps',
+      )) as google.maps.MapsLibrary
+
+      const map = new Map(mapRef.current, {
+        center: position,
+        disableDefaultUI: true,
+        zoom: 4,
+      })
+    })
+  }, [])
+
   return (
     <>
-      <div className={styles.map}>구글지도</div>
+      <div className={styles.map} ref={mapRef} />
       <button className={`${styles.guess}  ${themeStyles[theme + '-inverse']}`}>
         제출
       </button>
--- frontend/src/app/(main)/game/page.tsx
+++ frontend/src/app/(main)/game/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Timer from '@/app/components/Timer'
 import ThemeInfo from './_components/ThemeInfo'
 import GameInfo from './_components/GameInfo'
@@ -6,11 +8,31 @@ import Hints from './_components/Hints'
 import styles from './game.module.css'
 import themeStyles from '@/app/components/theme.module.css'
 import IngameMap from './_components/IngameMap'
+import StreetView from './_components/StreetView'
+
+import { Loader } from '@googlemaps/js-api-loader'
 
 export default function GamePage() {
+  //힌트
   const hints = ['hint 1', 'hint 2', 'hint 3']
+
+  //테마
   const theme = 'random'
+
+  //스테이지 시간
   const initialTime = 100
+
+  // 멀캠
+  const lat = 37.50155
+  const lng = 127.039374
+
+  //구글맵
+  const loader = new Loader({
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string,
+    version: 'weekly',
+    // ...additionalOptions,
+  })
+
   return (
     <main>
       <div className={styles.infos}>
@@ -27,9 +49,11 @@ export default function GamePage() {
         Chatting
       </div>
       <div className={`${styles.map} ${styles.opacity}`}>
-        <IngameMap theme={theme} />
+        <IngameMap theme={theme} loader={loader} />
       </div>
-      <div className={styles.streetView}>StreetView</div>
+      <div className={styles.streetView}>
+        <StreetView lat={lat} lng={lng} loader={loader} />
+      </div>
     </main>
   )
 }
