0d7a6290ff5c6f75105f224c06cda402580fb7f8
김다인 <frambo1se@naver.com>
2024-05-03 15:45:12 +0900

    [be] feat: SocketDTO 생성자 추가

--- backend/src/main/java/com/ssafy/be/common/model/dto/SocketDTO.java
+++ backend/src/main/java/com/ssafy/be/common/model/dto/SocketDTO.java
@@ -9,13 +9,24 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class SocketDTO {
-    private LocalDateTime senderDateTime;  // 송신자가 보낸 시간(Java 내 서버 시간)
+    private LocalDateTime senderDateTime;  // 송신자가 보낸 시간
     private String senderNickname;
     private int senderGameId;
     private int senderTeamId;
     private int code;
     private String msg;
 
+    // DTO 용도 사용 시 > 생성될 때 localdatetime.now()를 넣음
+    public SocketDTO(String senderNickname, int senderGameId, int senderTeamId) {
+        this.senderDateTime = LocalDateTime.now();
+        this.senderNickname = senderNickname;
+        this.senderGameId = senderGameId;
+        this.senderTeamId = senderTeamId;
+        this.code = 0;
+        this.msg = null;
+    }
+
+    // VO 용도 사용 시 > LocalDateTime을 지정하여 param으로 입력해야 함
     public SocketDTO(LocalDateTime senderDateTime, String senderNickname, int senderGameId, int senderTeamId, int code, String msg) {
         this.senderDateTime = senderDateTime;
         this.senderNickname = senderNickname;
@@ -25,7 +36,8 @@ public class SocketDTO {
         this.msg = msg;
     }
 
-    public void setCodeAndMsg(int code, String msg){
+
+    public void setCodeAndMsg(int code, String msg) {
         this.code = code;
         this.msg = msg;
     }
