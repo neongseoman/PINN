1048909affb1267fbc24ea5926fa5dd360cbcde7
김다인 <frambo1se@naver.com>
2024-05-03 16:54:26 +0900

    [be] feat: GameStatus 추가

--- backend/src/main/java/com/ssafy/be/common/component/GameComponent.java
+++ backend/src/main/java/com/ssafy/be/common/component/GameComponent.java
@@ -26,7 +26,7 @@ public class GameComponent {
     private LocalDateTime startedTime;
     private LocalDateTime finishedTime;
     private String password;
-    private String status;
+    private GameStatus status;
     private final int TeamCount = 10;
 
     private List<QuestionComponent> questions; // 이 게임에서 출제될 문제 목록
