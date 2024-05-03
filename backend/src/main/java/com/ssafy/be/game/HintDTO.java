b9814a48b6493f2356d9b3480ff80cc5b90decd7
박근수 <rmstn8426@gmail.com>
2024-05-03 16:59:37 +0900

    [be] feat: login redirect path edit

---
+++ backend/src/main/java/com/ssafy/be/game/HintDTO.java
@@ -0,0 +1,10 @@
+package com.ssafy.be.game;
+
+public class HintDTO {
+
+    private int elevator; //고도
+    private int temp; // 켈빈
+    private int pressure; // 기압
+    private int humidity; // 습도
+
+}
