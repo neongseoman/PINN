0a19fac4bba055a57cff5479161654473633048e
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-05-07 11:09:17 +0900

    [fe] feat: add persist

--- frontend/src/app/(main)/lobby/page.tsx
+++ frontend/src/app/(main)/lobby/page.tsx
@@ -1,7 +1,13 @@
 'use client'
 
+import useUserStore from '@/stores/userStore'
+
 export default function LobbyPage() {
 
+  const { gamerId, nickname } = useUserStore()
+
+  console.log(gamerId)
+  console.log(nickname)
   return (
     <main>
       <h1>로비 페이지</h1>
