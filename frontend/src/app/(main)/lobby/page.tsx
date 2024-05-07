fbb0c01815746dbc1d4d7e1a46c246d1d843c2ba
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-05-07 10:02:00 +0900

    [fe] feat: add zustand

--- frontend/src/app/(main)/lobby/page.tsx
+++ frontend/src/app/(main)/lobby/page.tsx
@@ -1,6 +1,12 @@
 'use client'
 
+import useUserStore from '@/stores/userStore'
+
 export default function LobbyPage() {
+  const { gamerId, nickname } = useUserStore()
+
+  console.log(gamerId)
+  console.log(nickname)
   // const router = useRouter();
   // const param = new URLSearchParams(window.location.search);
   // const token:any = param.get('code');
