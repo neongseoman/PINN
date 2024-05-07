1dcb75481b3cedcb5f1f70ee34169ccde0002092
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-05-07 11:10:23 +0900

    [fe] fix

--- frontend/src/app/(main)/lobby/page.tsx
+++ frontend/src/app/(main)/lobby/page.tsx
@@ -1,13 +1,7 @@
 'use client'
 
-import useUserStore from '@/stores/userStore'
-
 export default function LobbyPage() {
 
-  const { gamerId, nickname } = useUserStore()
-
-  console.log(gamerId)
-  console.log(nickname)
   return (
     <main>
       <h1>로비 페이지</h1>
