66ab15224d9cc918baeae3966a11d0bbdfb4b3f0
박근수 <rmstn8426@gmail.com>
2024-05-13 15:08:39 +0900

    [be] feat: 비밀방 제외

--- backend/src/main/java/com/ssafy/be/common/component/GameManager.java
+++ backend/src/main/java/com/ssafy/be/common/component/GameManager.java
@@ -272,6 +272,7 @@ public class GameManager {
         int maxGamers = -1;
 
         for (GameComponent game : games.values()){
+            if (game.getPassword() == null || "".equals(game.getPassword())) continue;
             if( game.getStatus() == GameStatus.READY){ // 시작 안한 게임
                 boolean possibleTeam = false;
                 boolean isPossibleGame = false;
