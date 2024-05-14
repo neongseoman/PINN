5955449aacefe285f36d899fd7a9fa6140ba5bab
김다인 <frambo1se@naver.com>
2024-05-14 23:28:27 +0900

    [be] feat: GamerLog, GamerStatus entity class 추가

---
+++ backend/src/main/java/com/ssafy/be/gamer/model/domain/GamerLog.java
@@ -0,0 +1,32 @@
+package com.ssafy.be.gamer.model.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class GamerLog {
+    @Id
+    private int gamerLogId;
+    private int gamerId;
+    private int gameId;
+    private int teamId;
+    private int rank;
+    private String teamColor;
+    private int isRoomLeader;
+    private int isTeamLeader; // 이거 근데 필요한가?ㅋㅋ
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+}
