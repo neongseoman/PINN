737e4bcf958907d74245fcc09a66134bdfaeb46a
진창현 <chang5189@naver.com>
2024-05-14 10:51:30 +0900

    [fe] feat: round-waiting
    Former-commit-id: 4c8696f63599ed9327a69ecc13467b9347137ebd

---
+++ frontend/src/app/(main)/game/[gameId]/[round]/waiting/page.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import styles from './roundResult.module.css'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { Loader } from '@googlemaps/js-api-loader'
+import RoundResultMap from './_components/RoundResultMap'
+import Timer from '@/components/Timer'
+
+
+interface RoundResult {
+  teamId: number
+  roundNumber: number
+  roundRank: number
+  totalRank: number
+  roundScore: number
+  totalScore: number
+  submitLat: number
+  submitLng: number
+}
+
+export default function LoadingPage({ params }: { params: { id: number } }) {
+  const [roundResult, setRoundResult] = useState<RoundResult[]>([])
+  const [count, setCount] = useState(5)
+  const router = useRouter()
+  const stageTime = 100
+
+  const loader = new Loader({
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string,
+    version: 'weekly',
+    // ...additionalOptions,
+  })
+
+  useEffect(() => {
+    const roundResultList = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/round/result`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('accessToken') as string
+              }`,
+          },
+        },
+      )
+
+      if (response.ok) {
+        console.log('라운드 결과 요청 통신 성공')
+        const responseData = await response.json()
+        if (responseData.code === 1000) {
+          console.log('라운드 결과 출력 성공!', responseData)
+          setRoundResult(responseData.result.roundResult)
+        } else {
+          console.log('라운드 결과 출력 실패!', responseData.code)
+          alert(responseData.message)
+        }
+      } else {
+        console.error('라운드 결과 요청 통신 실패', response)
+      }
+    }
+    roundResultList()
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount - 1)
+    }, 1000)
+
+    if (count === 0) {
+      clearInterval(timer)
+    }
+
+    return () => clearInterval(timer)
+  }, [count, router])
+
+  // api받아오기
+
+  return (
+    <main className={styles.background}>
+      <div className={styles.container}>
+        <div className={styles.round}>라운드 {1}</div>
+        <div className={styles.waiting}>
+          <Timer stageTime={stageTime} />
+        </div>
+        <div className={styles.mapWrapper}>
+          <RoundResultMap loader={loader} roundResult={roundResult} />
+        </div>
+        <div className={styles['rank-container']}>
+          <div className={styles.trophy}>
+            {roundResult.slice(0, 3).map((result, index) => (
+              <Image key={index} src={`/assets/images/svg/noto_${index + 1}-place-medal.svg`} alt="" width={28} height={28} />
+            ))}
+          </div>
+          <div className={styles.rank}>
+            {roundResult
+              .sort((a, b) => a.roundRank - b.roundRank)
+              .map(result => (
+                <div key={result.teamId} className={styles.teamList}>
+                  <div>{result.roundRank} TEAM {result.teamId}</div>
+                  <div>{result.roundScore.toLocaleString()}</div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
