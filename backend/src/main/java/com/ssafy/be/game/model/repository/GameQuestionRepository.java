ebb41cf7d04585f1cfeb86d908f2b2824c5d2df7
김다인 <frambo1se@naver.com>
2024-05-15 22:07:49 +0900

    [be] feat: GameQuestion 기본 CRUD 추가

--- backend/src/main/java/com/ssafy/be/game/model/repository/GameQuestionRepository.java
+++ backend/src/main/java/com/ssafy/be/game/model/repository/GameQuestionRepository.java
@@ -4,6 +4,11 @@ import com.ssafy.be.game.model.domain.GameQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameQuestionRepository extends JpaRepository<GameQuestion, Integer> {
+    List<GameQuestion> findAllByGameId(int gameId);
+
+    List<GameQuestion> findAllByQuestionId(int questionId);
 }
---
+++ backend/src/main/java/com/ssafy/be/game/service/GameQuestionService.java
@@ -0,0 +1,21 @@
+package com.ssafy.be.game.service;
+
+import com.ssafy.be.common.exception.BaseException;
+import com.ssafy.be.game.model.dto.entity.GameQuestionDTO;
+
+import java.util.List;
+
+public interface GameQuestionService {
+
+    int createGameQuestion(GameQuestionDTO gameQuestionDTO) throws BaseException;
+
+    int updateGameQuestion(GameQuestionDTO gameQuestionDTO) throws BaseException;
+
+    int deleteGameQuestion(int gameQuestionId) throws BaseException;
+
+    GameQuestionDTO getGameQuestion(int gameQuestionId) throws BaseException;
+
+    List<GameQuestionDTO> getAllGameQuestionByGameId(int gameId) throws BaseException;
+
+    List<GameQuestionDTO> getAllGameQuesitonByQuestionId(int questionId) throws BaseException;
+}
