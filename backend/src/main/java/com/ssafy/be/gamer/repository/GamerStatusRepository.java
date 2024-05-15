00079ac00847f5f87942c1e2c07bed9f441cc298
김다인 <frambo1se@naver.com>
2024-05-15 19:03:31 +0900

    [be] feat: GamerStatus 기본 CRUD+필요 메서드 구현 완료

--- backend/src/main/java/com/ssafy/be/gamer/repository/GamerStatusRepository.java
+++ backend/src/main/java/com/ssafy/be/gamer/repository/GamerStatusRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.be.gamer.repository;
 import com.ssafy.be.gamer.model.domain.GamerStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GamerStatusRepository extends JpaRepository<GamerStatus, Long> {
+public interface GamerStatusRepository extends JpaRepository<GamerStatus, Integer> {
 }
