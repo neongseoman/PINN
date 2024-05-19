9a8a3201ab5560b7cff489070f4059d55def6c46
sje08250 <sje08250@gmail.com>
2024-05-19 15:36:53 +0900

    [fe] feat: edit landing page
    Former-commit-id: 2cd3b142bd3fd724afece4b21f05ec3e95cf7d3b

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -15,65 +15,55 @@ export default function LandingPage() {
     window.Kakao.Auth.authorize({
       redirectUri: process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI,
     })
+    clickSound()
   }
 
-  const scrollDown = () => {
-    landingSound()
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    })
-  }
-
-  const scrollUp = () => {
-    // landingSound()
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    })
-  }
-
-  const landingSound = () => {
-    const audio = new Audio('/assets/sounds/landing.wav')
+  const hoverSound = () => {
+    const audio = new Audio('/assets/sounds/hover.wav')
     audio.play()
-    audio.loop = true
+  }
+
+  const clickSound = () => {
+    const audio = new Audio('/assets/sounds/click.mp3')
+    audio.play()
   }
 
   return (
     <>
       <main className={styles.landing}>
-        <p className={styles.name}>지도 게임</p>
         <img className={styles.logo} src="/assets/images/logo.png" alt="로고" />
-        <button className={styles.login} onClick={kakaoLogin}>
-          <img src="/assets/images/KakaoLogin.png" alt="카카오 로그인" />
-        </button>
-        <button className={styles.scroll} onClick={scrollDown}>
-          <FaAnglesDown />
-        </button>
         <div className={styles.guide}>
-          <div className={styles.left}>
+          <div className={styles.top}>
             <img
               className={styles.guide1}
               src="/assets/images/GuideLeft.png"
               alt="랜딩 페이지 설명 이미지 1"
             />
-            <p className={styles.text}>스트리트 뷰와 힌트를 참고해</p>
-            <p className={styles.text}>정답과 근접한 곳을 유추하여</p>
-            <p className={styles.text}>지도에 핀을 꽂아요!</p>
+            <div className={styles.textWrapper}>
+              <p className={styles.textTitle}>스트리트 뷰와 힌트를 참고해 위치를 유추하여 지도에 핀을 꽂아요!</p>
+              <p className={styles.text}>- 한 라운드 내에 두 개의 스테이지가 있어요</p>
+              <p className={styles.text}>- 라운드별로 하나의 장소를 맞히게 돼요</p>
+              <p className={styles.text}>- 스테이지1에서 스테이지2로 넘어가면 더 많은 힌트를 볼 수 있어요</p>
+              <p className={styles.text}>(스테이지1에서 정답을 맞히면 얻을 수 있는 최대 점수가 더 커요)</p>
+            </div>
           </div>
-          <button className={styles.scroll} onClick={scrollUp}>
-            <FaAnglesUp />
-          </button>
-          <div className={styles.right}>
+          <div className={styles.bottom}>
             <img
               className={styles.guide2}
               src="/assets/images/GuideRight.png"
               alt="랜딩 페이지 설명 이미지 2"
             />
-            <p className={styles.text}>정답과 가장 가까운 곳에</p>
-            <p className={styles.text}>핀을 꽂아서 보물을 획득해요!</p>
+            <div className={styles.textWrapper}>
+              <p className={styles.textTitle}>정답과 가장 가까운 곳에 핀을 꽂아서 우승해요!</p>
+              <p className={styles.text}>- 팀원들과 핀을 공유하며 답을 찾고, 확정된 답을 최대한 빠르게 제출해요</p>
+              <p className={styles.text}>- 우리 팀의 핀을 제출한 이후에는 다른 팀의 핀 위치를 볼 수 있어요</p>
+              <p className={styles.text}>- 제출한 핀의 위치와 정답 위치가 가까울수록 높은 점수를 획득해요</p>
+              <p className={styles.text}>- 라운드마다 얻은 점수를 합산하여 최종 우승 팀이 결정돼요</p>
+            </div>
           </div>
+          <p className={styles.textCenter}>더 자세한 설명은 로그인 후에 로비에서 확인할 수 있어요</p>
         </div>
+        <img className={styles.login} onClick={kakaoLogin} onMouseEnter={hoverSound} src="/assets/images/KakaoLogin.png" alt="카카오 로그인" />
       </main>
       <KakaoScript />
     </>
